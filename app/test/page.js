9256856727ad61f775373e11ef7ecea8748a8151
Waqas Obeidy <WObeidy@LT-Waqas-Obeidy.local>
2023-05-30 04:17:41 +0800

    mongodb added

--- app/test/page.js
+++ app/test/page.js
@@ -1,8 +1,33 @@
+// "use client";
+
+// import { useEffect, useState } from "react";
+
+// function page() {
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     fetch("api/test", { next: { revalidate: 5 } })
+//       .then((res) => res.json())
+//       .then((data) => setData(data))
+//       .catch((err) => console.log(err));
+//   }, []);
+
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+//         <p>{data.datetime}</p>
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default page;
+
 "use client";
 
 import { useEffect, useState } from "react";
 
-function page() {
+function DB() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -12,13 +37,14 @@ function page() {
       .catch((err) => console.log(err));
   }, []);
 
+  console.log(data);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p>{data.datetime}</p>
-      </div>
-    </main>
-  );
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+            <p>{data.datetime}</p>
+          </div>
+        </main>
+      );
 }
 
-export default page;
+export default DB;
